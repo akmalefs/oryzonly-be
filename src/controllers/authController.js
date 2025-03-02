b94a356243eeaea159188028176e80d272c924f2
yaelahmal <widad.akmal83@gmail.com>
2025-03-02 16:53:46 +0700

    feat: add login with google api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import bycrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import { google } from "googleapis";
+import crypto from "crypto";
 
 export const registerAction = async (req, res) => {
   try {
@@ -83,5 +85,100 @@ export const loginAction = async (req, res) => {
     return res.status(500).json({
       message: "Internal Server Error",
     });
+  }
+};
+
+export const loginWithGoogleAction = async (req, res) => {
+  try {
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_CALLBACK_URL
+    );
+
+    const scopes = [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ];
+
+    const url = oauth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: scopes,
+      include_granted_scopes: true,
+    });
+
+    return res.redirect(url);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const loginWithGoogleCallbackAction = async (req, res) => {
+  try {
+    const { code } = req.query;
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_CALLBACK_URL
+    );
+
+    const { tokens } = await oauth2Client.getToken(code);
+
+    oauth2Client.setCredentials(tokens);
+
+    const oauth2 = google.oauth2({
+      auth: oauth2Client,
+      version: "v2",
+    });
+
+    const { data } = await oauth2.userinfo.get();
+
+    if (!data.email || !data.name) {
+      return res.json({
+        data: data,
+      });
+    }
+
+    let user = await userModel.findOne({ email: data.email }).lean();
+
+    if (!user) {
+      try {
+        user = await userModel.create({
+          name: data.name,
+          email: data.email,
+          password: crypto.randomBytes(16).toString("hex"),
+        });
+      } catch (error) {
+        if (error.code === 11000) {
+          user = await userModel.findOne({ email: data.email }).lean(); // Ambil ulang user
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    const token = jwt.sign(
+      {
+        data: {
+          id: user._id.toString(),
+          name: user.name,
+          email: user.email,
+        },
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    return res.json({
+      message: "Login success",
+      data: {
+        name: user.name,
+        email: user.email,
+        token,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { validateRequest } from "../middlewares/validateRequest.js";
 import { loginSchema, registerSchema } from "../utils/schema.js";
-import { loginAction, registerAction } from "../controllers/authController.js";
+import {
+  loginAction,
+  loginWithGoogleAction,
+  loginWithGoogleCallbackAction,
+  registerAction,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", validateRequest(registerSchema), registerAction);
 router.post("/login", validateRequest(loginSchema), loginAction);
+router.get("/auth/google", loginWithGoogleAction);
+router.get("/auth/google/callback", loginWithGoogleCallbackAction);
 
 export default router;
